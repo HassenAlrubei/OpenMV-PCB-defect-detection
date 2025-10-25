2cc6747d15b5c90c3d1bc64ab9aed4278adbe273
Hassen <hhass7786@gmail.com>
2025-10-25 16:16:08 +0100

    inefficiently joined all txt file info into one print json format

--- json_conv.py
+++ json_conv.py
@@ -1,48 +1,64 @@
 import json
 import os
+import pprint
+import time
+
+from tqdm import tqdm
+
 
 training_labels = 'C:/Users/admin/Documents/3rd Year project/OpenMV/PCB_DATASET/Train Valid Test/train/labels'
-
 main_dict = {}
-
-#main_fields = ['version', 'files']
 files_keys = ['filename', 'category', 'label', 'boundingBoxes']
-
-
 
 main_dict['version'] = 1
 main_dict['files'] = []
 
-boundingBox_info_list = ['label', 'x', 'y', 'width', 'height']
-boundingBox_info_dict = dict.fromkeys(boundingBox_info_list)
+boundingBox_info_list = ['ID', 'x', 'y', 'width', 'height']
 
 for file in os.scandir(training_labels):
     if file.is_file():
-        new_intra_files_dict = dict.fromkeys(files_keys) # remember to append this to the main_dict['files'] list
 
-        # main dictionary for each image file 
+        new_intra_files_dict = dict.fromkeys(files_keys) # remember to append this to the main_dict['files'] list --> **DONE**
         new_intra_files_dict['filename'] = os.path.basename(file)
-        new_intra_files_dict['Label'] = {} # read each txt file and return the id -> that will be the label, e.g. mouse_bite
-        new_intra_files_dict['boundingBoxes'] = [] #read each file and return coordinates and its width and height
         new_intra_files_dict['category'] = "Training" # change for testing & validating
+        new_intra_files_dict['label'] = {} # read each txt file and return the id -> that will be the label, e.g. mouse_bite
+        new_intra_files_dict['boundingBoxes'] = [] 
 
         # intra-label dictionary
-        new_intra_files_dict['Label']['type'] = "label"
-        new_intra_files_dict['Label']['label'] = "defect"
+        new_intra_files_dict['label']['type'] = "label"
+        new_intra_files_dict['label']['label'] = "defect"
 
-        pretty_data = json.dumps(new_intra_files_dict, indent = 4)
-        main_dict['files'].append(pretty_data)
-
-       # print(main_dict)
-
-        i = 0
+        
+        # Iterate through each line in each txt file, creating a dictionary for each line
         with open(file) as fh:
             for line in fh:
-                new_intra_files_dict['boundingBoxes'].append(boundingBox_info_dict)
+
+                boundingBox_dict = {}
                 info = list(line.strip().split())
-                while i<len(boundingBox_info_list):
-                    boundingBox_info_dict[i] = info[i]
-                    i = i + 1
-                  
-            
-        print(boundingBox_info_dict)
+                
+
+                for i, j in zip(boundingBox_info_list, info):
+                    boundingBox_dict.update({i: j})
+
+                    if (info[0] == '0'):
+                        boundingBox_dict['ID'] = "Mouse_Bite"
+                    elif (info[0] == '1'):
+                        boundingBox_dict['ID'] = "Spur"
+                    elif (info[0] == '2'):
+                        boundingBox_dict['ID'] = "Missing_Hole"
+                    elif (info[0] == '3'):
+                        boundingBox_dict['ID'] = "Short_Circuit"
+                    elif (info[0] == '4'):
+                        boundingBox_dict['ID'] = "Open_Ciruit"
+                    elif (info[0] == '5'):
+                        boundingBox_dict['ID'] = "Spurious_Copper"
+
+                    
+                    time.sleep(0.005)
+
+                new_intra_files_dict['boundingBoxes'].append(boundingBox_dict)
+
+                pretty_data = json.dumps(new_intra_files_dict, indent = 4)
+                main_dict['files'].append(pretty_data)
+                
+                pprint.pp(main_dict)
